284b31233b8306dbb29c543b9b66b2d8f9e5f80c
samed232323 <samed.hbo23@gmail.com>
2022-02-14 11:54:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle;
+            Console.Write("Bir cümle giriniz: ");
+            cumle = Console.ReadLine();
+            // bir strin ifadenin karakter sayısını Length 
+            //Console.WriteLine(cumle.Length); 
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                Console.WriteLine(cumle[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
